01847338dbd8c1517287b9f170558b327e92a1ff
Ben Smith <ben@parthenonsoftware.com>
2013-02-01 12:48:52 -0800

    Added country and admin1 (SGU) to Geoname list view in admin.py

--- geonames/admin.py
+++ geonames/admin.py
@@ -1,7 +1,9 @@
 from django.contrib.gis import admin
 from models import Geoname
 
+
 class GeonameAdmin(admin.OSMGeoAdmin):
     search_fields = ('name',)
-    
+    list_display = ('name', 'country', 'admin1',)
+
 admin.site.register(Geoname, GeonameAdmin)
